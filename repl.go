832724ae5e5d22d7c6a0f8ba358e56e9155a70cc
evlbit <evlbit@proton.me>
2025-02-05 09:20:06 +0200

    Base repl functionality

--- repl.go
+++ repl.go
@@ -1,6 +1,31 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Print("Pokedex > ")
+	for scanner.Scan() {
+		words := cleanInput(scanner.Text())
+
+		if len(words) > 0 {
+			fmt.Println("Your command was:", words[0])
+		}
+
+		fmt.Print("Pokedex > ")
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatalf("error reading stdin: %v", err)
+	}
+}
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
